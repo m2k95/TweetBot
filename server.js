764d093fe9f1c446121695501dc3f8113ad025fc
mfilkawi <m.filkawi@gmail.com>
2020-06-10 11:19:25 +0300

    web server created

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
+const fs = require('fs');
+const serveIndex = require('serve-index');
+const app = express();
+const logsPath = path.join(__dirname, 'logs/');
+
+app.use(cors());
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+    res.json({
+        message: "Hello Wolrd!"
+    })
+})
+
+app.use('/logs', serveIndex(logsPath));
+
+app.use('/', (req, res) => {
+    var fullURL = req.protocol + '://' + req.get('host') + req.originalUrl;
+    var r = fullURL.split('/').reverse();
+
+    fs.readFile(`./logs/${r[1]}/${r[0]}`, (e, data) => {
+        if (e) throw e;
+        console.log(data);
+        res.send(data.toString());
+    });
+});
+
+app.listen(3000, () => {
+    console.log('Server started at port http://0.0.0.0:3000');
+});
